2b27f8f9341535901a72f8f7a97d442aff1c9d30
advaiyalad <advaiya.lad@gmail.com>
2021-01-29 17:14:59 -0600

    Add skeleton css and Lato font

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,10 @@
 <script lang="ts">
 </script>
+
+<!-- This is test code, not part of the real application -->
+<h1>TEST</h1>
+<div class="row">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+</div>
